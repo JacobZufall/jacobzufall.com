28573b64ef295522a76c3d06f934bf51b8d19cd4
jacobzufall <jacobzufall@gmail.com>
2024-01-17 21:06:23 -0500

    Working on keeping themes applicable across all pages

--- src/script.js
+++ src/script.js
@@ -1,23 +1,34 @@
-const theme = document.querySelector("#themeLink");
 let darkMode = false;
 
 /**
- * Toggles the website between dark and light mode. This doesn't currently carry between pages which is a big issue
- * because I don't want visitors to have to press the button on each page they visit.
- * @returns Nothing.
+ * Checks if dark mode is enabled or not to keep settings consistent across pages.
+ * @returns void
  */
-function ChangeTheme() {
+function CheckTheme() {
     const sun = document.querySelector("#sun");
     const moon = document.querySelector("#moon");
 
     if (darkMode) {
         moon.style.display = "inline";
         sun.style.display = "none";
-        darkMode = false;
     } else {
         moon.style.display = "none";
         sun.style.display = "inline";
+    }
+}
+
+/**
+ * Toggles the website between dark and light mode. This doesn't currently carry between pages which is a big issue
+ * because I don't want visitors to have to press the button on each page they visit.
+ * @returns void
+ */
+function ChangeTheme() {
+    if (darkMode) {
+        darkMode = false;
+        CheckTheme();
+    } else {
         darkMode = true;
+        CheckTheme();
     }
 
     document.body.classList.toggle("darkMode")
